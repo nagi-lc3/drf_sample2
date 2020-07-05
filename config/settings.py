731374f91c2a03aaed9fd561afda158d2d6eee90
nagi <ymxlinkinparkxlc@gmail.com>
2020-07-06 00:10:18 +0900

    Fix error(settings.py)

--- config/settings.py
+++ config/settings.py
@@ -216,7 +216,7 @@ INSTALLED_APPS += (
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
 
